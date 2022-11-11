67dffdf4a5b1c7ad6e10d843bbc33e62029343a6
Amie Wei <amie.wei12@gmail.com>
2022-11-10 19:54:47 -0600

    play to 5

--- app.js
+++ app.js
@@ -3,35 +3,51 @@
 
 // const btnArray = ['.btnRock', '.btnPaper', '.btnScissors'];
 
-let playerScore = 0;
-let computerScore = 0;
+// let playerScore = 0;
+// let computerScore = 0;
 
-const buttons = document.querySelectorAll('.btn')
+const buttons = document.querySelectorAll('.button')
+const gameButtons = document.querySelectorAll('.btnGame')
+console.log(gameButtons)
+
+const player = {
+    score : 0,
+    display: document.querySelector('#p1Display')
+}
+
+const computer = {
+    score : 0,
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('.resetBtn');
 // console.log(buttons)
-
-buttons.forEach(btn => btn.addEventListener('click', (evt) => {
-    playerSelection = evt.target.innerText.toLowerCase()
-    playGame(playerSelection)
-}));
-
-    
+ 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         console.log(`it's a tie. Noone gets a point`)
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        console.log(`computerScore: ${++computerScore}`)
+        computer.score += 1;
+        computer.display.innerText = computer.score
+        console.log(`computerScore: ${computer.score}`)
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log(`computerScore: ${++computerScore}`)
+        computer.score += 1;
+        computer.display.innerText = computer.score
+        console.log(`computerScore: ${computer.score}`)
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        console.log(`computerScore: ${++computerScore}`)
+        computer.score += 1;
+        computer.display.innerText = computer.score
+        console.log(`computerScore: ${computer.score}`)
     } else {
-        console.log(`playerScore: ${++playerScore}`)
+        player.score += 1;
+        player.display.innerText = player.score
+        console.log(`playerScore: ${player.score}`)
     }
+
 }
 
 function getComputerChoice(array) {
         // get random index value
-
     let randomIndex = Math.floor(Math.random() * array.length);
     // get random item
     let computerSelection = array[randomIndex];
@@ -41,23 +57,64 @@ function getComputerChoice(array) {
 
 function playGame(playerSelection){
     const array = ['rock', 'paper', 'scissors'];
-
-    // for (let i = 0; i < 5; i++) {
-
     let computerSelection = getComputerChoice(array);
-    // let playerSelection = selection.toLowerCase()
-
+    
     console.log(`playerSelection: ${playerSelection}, computerSelection: ${computerSelection}`)
-    
-    console.log(playRound(playerSelection, computerSelection));
-    
-    if (playerScore == 5 || computerScore == 5) {
-        if (playerScore > computerScore) {
-            console.log(`You Won! Final Score - player: ${playerScore}, computer: ${computerScore}`)
+    playRound(playerSelection, computerSelection);
+
+    if (player.score == 5 || computer.score == 5) {
+        if (player.score > computer.score) {
+            console.log(`You Won! Final Score - player: ${player.score}, computer: ${computer.score}`)
+            player.display.classList.add('winner')
+            computer.display.classList.add('loser')
         } else {
-            console.log(`Computer Wins :( Final Score - player: ${playerScore}, computer: ${computerScore}`)
+            console.log(`Computer Wins :( Final Score - player: ${player.score}, computer: ${computer.score}`)
+            computer.display.classList.add('winner')
+            player.display.classList.add('loser')
         }
+        gameButtons.forEach(btn => {btn.disabled = true});
+        gameButtons.forEach(btn => {btn.classList.add('selection-disabled')});
+
+
     }
 
 }
 
+function reset() {
+    for (let p of [player, computer]) {
+        p.score = 0;
+        p.display.innerText = p.score
+        p.display.classList.remove('winner', 'loser');
+        // p.button.disabled = false;
+    }
+    gameButtons.forEach(btn => {btn.disabled = false});
+    gameButtons.forEach(btn => {btn.classList.remove('selection-disabled')});
+    console.log(player.score, player.score);
+}
+
+function removeTransition(evt) {
+    // console.log(evt)
+    if (evt.propertyName !== 'transform') return;
+    //this is else, so if propertyname is transform
+    // console.log(evt.propertyName)
+    this.classList.remove('playing')
+};
+
+
+buttons.forEach(btn => btn.addEventListener('mousedown', (evt) => {
+    playerSelection = evt.target.innerText.toLowerCase()
+    console.log(playerSelection)
+
+    // not this as this here refers to window
+    btn.classList.add('playing');
+
+    if (playerSelection == "reset") {
+        reset()
+    } else {
+        playGame(playerSelection)
+    }
+}));
+
+
+// listening for transition end
+buttons.forEach(btn => btn.addEventListener('transitionend', removeTransition));
